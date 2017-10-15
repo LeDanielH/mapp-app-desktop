cca8bc9b6aebc4bbacd9ca80bd0c15c1c6ce80ce
Daniel Zeman <ledaniel.zeman@gmail.com>
2017-10-15 04:19:17 +0200

    171015 - new UA template

---
+++ src/data/ua.js
@@ -0,0 +1,10 @@
+module.exports ={
+	title: `User Agreement`,
+	content: `<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci at aut consectetur doloremque 
+				eos fugit id in ipsam nam nostrum officia, pariatur provident quis quo recusandae sit tenetur voluptatem. 
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p> <p>Alias amet atque blanditiis consectetur corporis 
+				cumque dolorem ducimus eum illo iste magnam molestias non odio optio possimus reiciendis repellat, 
+				vitae voluptatibus?</p><br><p>Alias amet atque blanditiis consectetur corporis 
+				cumque dolorem ducimus eum illo iste magnam molestias non odio optio possimus reiciendis repellat, 
+				vitae voluptatibus?</p>`
+};
