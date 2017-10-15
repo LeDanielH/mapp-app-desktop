3c1219803443961037e055cc17bff38ea12b9015
Daniel Zeman <ledaniel.zeman@gmail.com>
2017-10-15 03:08:02 +0200

    171015 - modal script edit after isses in IE

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,55 +6,31 @@ const Modal = {
 	scrollDelay: 300,
 	currentAnswer: null,
 
-	getAnwser(e) {
-		let matchesQuestion = e.target.matches ? e.target.matches('.question__item') : event.target.msMatchesSelector('.question__item');
-		if (!matchesQuestion) return;
-		e.preventDefault();
-		const question = e.target;
-		this.saveCurrentScroll(question);
+	getAnwser(question) {
 		const category = parseInt(question.dataset.category);
 		const order = parseInt(question.dataset.order);
 		this.currentAnswer = document.getElementById(`answer-${category}-${order}`);
-		const wrapper = document.getElementById('mapp__wrapper');
 		this.currentAnswer.classList.add('active');
-		wrapper.classList.add('active');
-		if (typeof window.scroll !== "undefined") {
-			setTimeout(()=> {
-				window.scroll({
-					top: 0,
-					behavior: 'smooth',
-					duration: this.scrollDelay
-				});
-			}, this.scrollDelay);
-		}
-
-
 	},
 
-	saveCurrentScroll() {
-		this.currentScroll = Math.max(document.body.scrollTop,document.documentElement.scrollTop, window.pageYOffset);
-	},
-
-	closeAnswer() {
-		if(this.currentAnswer === null) return;
-		const wrapper = document.getElementById('mapp__wrapper');
+	closeAnswer(e) {
+		let matchesAnswer = e.target.matches ? e.target.matches('.answer__close') : event.target.msMatchesSelector('.answer__close');
+		if (!matchesAnswer) return;
+		e.preventDefault();
+		const closeButton = e.target;
+		const category = parseInt(closeButton.dataset.category);
+		const order = parseInt(closeButton.dataset.order);
+		this.currentAnswer = document.getElementById(`answer-${category}-${order}`);
 		this.currentAnswer.classList.remove('active');
-		this.currentAnswer = null;
-		wrapper.classList.remove('active');
-		if ((typeof window.scroll !== "undefined") && (this.currentScroll > 0)) {
-			setTimeout(()=> {
-				window.scroll({
-					top: this.currentScroll,
-					behavior: 'smooth'
-				});
-			}, this.scrollDelay);
-		}
 
 	},
 	init() {
-		document.addEventListener('click', (e) => this.getAnwser(e));
-		const arrow = document.getElementById('app__arrow');
-		arrow.addEventListener('click', () => this.closeAnswer());
+		const questions = document.querySelectorAll('.question__wrapper');
+
+		for(let i = 0; i < questions.length; i++) {
+			questions[i].addEventListener('click', () => this.getAnwser(questions[i]))
+		}
+		document.addEventListener('click', (e) => this.closeAnswer(e));
 	}
 };
 
